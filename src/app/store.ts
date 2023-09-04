2ad30278a4f017198cb966c392e300d683ecd26d
Julia <veluat@gmail.com>
2023-09-04 11:36:19 +0300

    Initial commit

---
+++ src/app/store.ts
@@ -0,0 +1,36 @@
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {appReducer} from './app-reducer'
+import {authReducer} from '../features/Login/auth-reducer'
+import createSagaMiddleware from 'redux-saga'
+import { takeEvery } from 'redux-saga/effects'
+
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer
+})
+
+const sagaMiddleware = createSagaMiddleware()
+// непосредственно создаём store
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware));
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+sagaMiddleware.run(rootWatcher)
+
+function* rootWatcher() {
+yield takeEvery('ACTIVATOR-ACTION-TYPE', rootWorker)
+}
+
+function* rootWorker() {
+
+}
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
