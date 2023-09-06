da0f67f02f5b2205de536ff2c6f9204d649ee9f7
Julia <veluat@gmail.com>
2023-09-06 18:40:03 +0300

    fix: app on the saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,17 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import { tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import {appReducer} from './app-reducer'
+import {appReducer, initializeAppWorkerSaga} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
 import createSagaMiddleware from 'redux-saga'
-import { takeEvery } from 'redux-saga/effects'
+import {takeEvery} from 'redux-saga/effects'
+import {
+    addTaskWorkerSaga,
+    fetchTasksWorkerSaga,
+    removeTaskWorkerSaga, tasksWatcherSaga
+} from "../features/TodolistsList/Todolist/tasks-sagas";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
@@ -17,20 +20,18 @@ const rootReducer = combineReducers({
 })
 
 const sagaMiddleware = createSagaMiddleware()
-// непосредственно создаём store
+
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware));
-// определить автоматически тип всего объекта состояния
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 sagaMiddleware.run(rootWatcher)
 
 function* rootWatcher() {
-yield takeEvery('ACTIVATOR-ACTION-TYPE', rootWorker)
+    yield takeEvery("APP/INITIALED-APP", initializeAppWorkerSaga)
+    yield tasksWatcherSaga()
 }
 
-function* rootWorker() {
 
-}
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
